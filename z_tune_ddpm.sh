4dd86970df36622065a141b7b864b52dc01e940b
Conor R. Warren <cw0466@della-gpu.princeton.edu>
2025-05-01 10:45:46 -0400

    Update

--- z_tune_ddpm.sh
+++ z_tune_ddpm.sh
@@ -8,7 +8,7 @@
 #SBATCH --mem=2G                 # total memory
 #SBATCH --gres=gpu:1             # number of gpus per node
 #SBATCH --mem=2G                 # total memory
-#SBATCH --time=10:00:00          # total run time limit (HH:MM:SS)
+#SBATCH --time=12:00:00          # total run time limit (HH:MM:SS)
 #SBATCH --mail-type=begin        # send email when job begins
 #SBATCH --mail-type=end          # send email when job ends
 #SBATCH --mail-type=fail         # send email if job fails
@@ -19,4 +19,4 @@ module purge
 module load anaconda3/2023.9
 conda activate tddpm
 
-python scripts/tune_ddpm.py breast_cancer catboost cv cuda
+python scripts/tune_ddpm.py breast_cancer 409 synthetic catboost ddpm_tune --eval_seed
