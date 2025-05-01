33dae8e9bc99ca9202b784107c1c3e15adeb8c1c
Conor R. Warren <cw0466@della-gpu.princeton.edu>
2025-05-01 13:10:30 -0400

    Added evaluation script

---
+++ z_tune_eval.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+#SBATCH --job-name=tune_eval_heart_failure  # create a short name for your job
+#SBATCH --nodes=1                # node count
+#SBATCH --ntasks=1               # total number of tasks across all nodes
+#SBATCH --cpus-per-task=1        # cpu-cores per task (>1 if multi-threaded tasks)
+#SBATCH --partition=mig
+#SBATCH --mem=2G                 # total memory
+#SBATCH --gres=gpu:1             # number of gpus per node
+#SBATCH --mem=2G                 # total memory
+#SBATCH --time=12:00:00          # total run time limit (HH:MM:SS)
+#SBATCH --mail-type=begin        # send email when job begins
+#SBATCH --mail-type=end          # send email when job ends
+#SBATCH --mail-type=fail         # send email if job fails
+#SBATCH --mail-user=cw0466@princeton.edu
+
+# Set up environment
+module purge
+module load anaconda3/2023.9
+conda activate tddpm
+
+python scripts/tune_evaluation_model.py heart_failure catboost cv cuda
