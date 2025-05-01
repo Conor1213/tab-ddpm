01a2d7d425a266a6f1eaa0260fcdd44911e47c7a
Conor R. Warren <cw0466@della-gpu.princeton.edu>
2025-05-01 09:48:52 -0400

    Cleaned up repo. Tuned evaluation model on breast cancer

--- scripts/tune_evaluation_model.py
+++ scripts/tune_evaluation_model.py
@@ -12,7 +12,7 @@ parser.add_argument('tune_type', type=str)
 parser.add_argument('device', type=str)
 
 args = parser.parse_args()
-data_path = Path(f"data/{args.ds_name}")
+data_path = Path(f"/home/cw0466/ece477-final-project/data/{args.ds_name}/authentic")
 best_params = None
 
 assert args.tune_type in ("cv", "val")
